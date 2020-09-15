55c20fcfe3f0ae2e977bf0c1fbf43a6a083845c5
Rafael Hirooka <rafaelhirooka@hotmail.com>
2020-09-14 23:00:26 -0300

    sign up and unform

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { Container, Content, Background } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+
+          <Input name="name" placeholder="Nome" icon={FiUser} />
+          <Input name="email" placeholder="E-mail" icon={FiMail} />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Senha"
+            icon={FiLock}
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para o logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
